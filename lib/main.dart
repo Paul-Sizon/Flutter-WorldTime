19a087756604efa2a9c4a236123acf60f91d8404
Paul <p.sizonenko@alfaleads.net>
2021-02-24 17:53:39 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:worldtime/pages/chooseLocation.dart';
+import 'package:worldtime/pages/home.dart';
+import 'package:worldtime/pages/loading.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
+  ));
+}
+
+
---
+++ lib/test.dart
@@ -0,0 +1,4 @@
+void main() {
+  Map student = {'name': "Paul", "age": 23};
+  print(student['age']);
+}
